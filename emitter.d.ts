cab676f5dd0eb12dcbda753e3f6206af1667fb66
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2023-06-07 09:16:24 +0300

    update sequential event emitter (#152)
    * update sequential event emitter
    
    * 2.10.2

--- emitter.d.ts
+++ emitter.d.ts
@@ -23,9 +23,24 @@ export declare interface SequentialEventEmitterBase {
  * @class
  * @extends EventEmitter
  */
-export declare class SequentialEventEmitter implements SequentialEventEmitterBase extends EventEmitter {
+export declare class SequentialEventEmitter extends EventEmitter implements SequentialEventEmitterBase {
     /**
      * @constructor
      */
     constructor();
+    emit(event: string | symbol, ...args: any[]): any;
+    addListener(event: string | symbol, listener: (...args: any[]) => void): this;
+    on(event: string | symbol, listener: (...args: any[]) => void): this;
+    prependListener(event: string | symbol, listener: (...args: any[]) => void): this;
+    prependOnceListener(event: string | symbol, listener: (...args: any[]) => void): this;
+    removeListener(event: string | symbol, listener: (...args: any[]) => void): this;
+    removeAllListeners(event?: string | symbol): this;
+    setMaxListeners(n: number): this;
+    getMaxListeners(): number;
+    listenerCount(type: string | symbol): number;
+    once(event: string | symbol, listener: (...args: any[]) => void): this;
+    subscribe(event: string | symbol, asyncListener: (...args: any[]) => Promise<void>): this;
+    unsubscribe(event: string | symbol, asyncListener: (...args: any[]) => Promise<void>): this;
+    subscribeOnce(event: string | symbol, asyncListener: (...args: any[]) => Promise<void>): this;
+    next(event: string | symbol, ...args: any[]): Promise<void>;
 }
